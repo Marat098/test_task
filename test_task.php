012c88ee216912553dda0b2faced652d85589236
Marat098 <78295458+Marat098@users.noreply.github.com>
2023-07-31 19:36:08 +0600

    add class

--- test_task.php
+++ test_task.php
@@ -1,19 +1,47 @@
 <?php
-$paths = [];
-function search_path($from, $arrival_time, $path, $flights){
-	foreach( $flights as $key => $value ){
-		$date_1 = date( strtotime($value['arrival']));
-		$date_2 = date( strtotime($arrival_time));
-		if( $value['from'] == $from and $date_1 >= $date_2){
-			$obj =  new ArrayObject($path);
-			$new_path = $obj->getArrayCopy();
-			array_push($new_path, $value);
-			search_path($value['to'], $value['arrival'], $new_path, $flights);
-		}
+class FlightsInfo
+{
+    
+    public $paths = []; 
+    public $flights = []; 
+    public $long_path = []; 
+    
+    public function __construct($flights){
+	    $this->flights = $flights;
 	}
-	$path['time'] = date( strtotime(end($path)['arrival'])) - date( strtotime($path[0]['depart']));
-	array_push($GLOBALS['paths'], $path);
+
+    public function search_path($from, $arrival_time, $path){
+        foreach( $this->flights as $key => $value ){
+			$date_1 = date( strtotime($value['arrival']));
+			$date_2 = date( strtotime($arrival_time));
+			if( $value['from'] == $from and $date_1 >= $date_2){
+				$obj =  new ArrayObject($path);
+				$new_path = $obj->getArrayCopy();
+				array_push($new_path, $value);
+				$this->search_path($value['to'], $value['arrival'], $new_path);
+			}
+		}
+		$path['time'] = date( strtotime(end($path)['arrival'])) - date( strtotime($path[0]['depart']));
+		array_push($this->paths, $path);
+    }
+    
+    public function search_longest_path(){
+    	$this->long_path = $this->paths[0];
+
+		foreach( $this->paths as $key => $value ){
+			if ($value['time'] > $this->long_path['time']){
+				$this->long_path = $value;
+			}
+		}
+    }
+    
+    public function start_search(){
+    	foreach( $this->flights as $key => $value ){
+			$this->search_path($value['to'], $value['arrival'], [$value]);
+		}
+    }
 }
+
 
 $flights = [
 	[
@@ -36,15 +64,9 @@ $flights = [
 	],
 ];
 
-foreach( $flights as $key => $value ){
-	search_path($value['to'], $value['arrival'], [$value], $flights);
-}
-$long_path = $paths[0];
+$flightsInfo = new FlightsInfo($flights);
+$flightsInfo->start_search();
+$flightsInfo->search_longest_path();
 
-foreach( $paths as $key => $value ){
-	if ($value['time'] > $long_path['time']){
-		$long_path = $value;
-	}
-}
 
-print_r($long_path);
+print_r($flightsInfo->long_path);
